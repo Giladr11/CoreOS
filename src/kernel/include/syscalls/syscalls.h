b18e9cc468fbb308fe8cccff69d0d4bf00b07a5d
giladr11 <gilad.rafael2007@gmail.com>
2026-01-30 05:06:48 +0200

    removed some marks

--- src/kernel/include/syscalls/syscalls.h
+++ src/kernel/include/syscalls/syscalls.h
@@ -3,11 +3,11 @@
 
 #include "include/arch/x86/isr/isr.h"
 
-#define SYS_HEAP_DUMP   0x01 // 1
-#define SYS_HEAP_ALLOC  0x02 // 2
-#define SYS_HEAP_FREE   0x03 // 3
-#define SYS_BEEP        0x04 // 4
-#define SYS_PLAY_MELODY 0x05 // 5
+#define SYS_HEAP_DUMP   0x01 
+#define SYS_HEAP_ALLOC  0x02 
+#define SYS_HEAP_FREE   0x03 
+#define SYS_BEEP        0x04 
+#define SYS_PLAY_MELODY 0x05 
 
 // Syscalls Functions
 void sys_heap_dump(Registers* regs);
@@ -17,7 +17,7 @@ void sys_beep(Registers* regs);
 void sys_play_melody(Registers* regs);
 
 typedef struct {
-    uint32_t value;     // actual return  value
+    uint32_t value;     // actual return value
     uint8_t  status;    // 0 = fail, 1 = ok
 } SyscallResult;
 
